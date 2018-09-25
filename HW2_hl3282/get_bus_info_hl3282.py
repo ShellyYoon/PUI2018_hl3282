3e050ec75e11c21702c0252056b783c860e96c6f
李晗星 <lihanxing@ris-MacBook-puro.local>
2018-09-25 17:18:22 -0400

    1st upload

---
+++ HW2_hl3282/get_bus_info_hl3282.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 25 15:07:39 2018
+
+@author: lihanxing
+"""
+from __future__ import print_function
+import json 
+try:
+    import urllib2 as urllib
+except ImportError:
+    import urllib.request as urllib
+import os
+import sys 
+apikey=sys.argv[1]
+busline=sys.argv[2]
+csv=sys.argv[3]
+if not len(sys.argv) == 4:
+    print("invalid number of arguments. Please input 3 arguments like: python get_bus_info_hl3282 <MTA_KEY> <BUS_LINE> <BUS_LINE.csv>")
+    sys.exit()
+mtaurl="http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=%s&VehicleMonitoringDetailLevel=calls&LineRef=%s"%(apikey,busline)
+print (mtaurl)
+
+
+response = urllib.urlopen(mtaurl)
+data = response.read().decode("utf-8")
+dataDict = json.loads(data)
+
+outputcsvfile=open(csv,"w")
+outputcsvfile.write("Latitude,Longitude,Stop Name,Stop Status\n")
+buslists = dataDict['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']
+for i in range(0,len(buslists)):
+    latitude = buslists[i]['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
+    longitude = buslists[i]['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
+    try:
+        stop_name = buslists[i]['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]['StopPointName']
+        stop_status = buslists[i]['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]['Extensions']['Distances']['PresentableDistance']
+    except LookupError:
+        stop_name = "N/A"
+        stop_status = "N/A"
+    outputcsvfile.write("%s,%s,%s,%s\n"%(latitude,longitude,stop_name,stop_status))
+outputcsvfile.close()
+
---
+++ HW2_hl3282/show_bus_locations_hl3282.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 23 21:06:54 2018
+
+@author: lihanxing
+"""
+from __future__ import print_function
+import json 
+try:
+    import urllib2 as urllib
+except ImportError:
+    import urllib.request as urllib
+import os
+import sys 
+apikey=sys.argv[1]
+busline=sys.argv[2]
+if not len(sys.argv) == 3:
+    print("invalid number of arguments. Please input 2 arguments like: python show_bus_locations_hl3282.py <MTA_KEY> <BUS_LINE>")
+    sys.exit()
+mtaurl="http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=%s&VehicleMonitoringDetailLevel=calls&LineRef=%s"%(apikey,busline)
+
+print(mtaurl)
+response = urllib.urlopen(mtaurl)
+data = response.read().decode("utf-8")
+dataDict = json.loads(data)
+
+buslists = dataDict['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']
+print("Bus Line : %s"%(busline))
+print("Number of Active Buses : %s"%(len(buslists)))
+for i in range(0,len(buslists)):
+    latitude=buslists[i]['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
+    longitude=buslists[i]['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
+    print("Bus %s is at latitude %s and longitude %s"%(i,latitude,longitude))
